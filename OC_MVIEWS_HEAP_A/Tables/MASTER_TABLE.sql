73ccb276720711d58d622f14900e78fcc983bfff
Łukasz Ciszewski <ciszewski@tkomp.pl>
2017-03-20 08:42:49 +0100

    tt

--- OC_MVIEWS_HEAP_A/Tables/MASTER_TABLE.sql
+++ OC_MVIEWS_HEAP_A/Tables/MASTER_TABLE.sql
@@ -1,6 +1,6 @@
 CREATE TABLE oc_mviews_heap_a.master_table (
   amout NUMBER(12) NOT NULL,
   nottheanswerff NUMBER(38),
-  testing2 NUMBER(318),
+  testing232 NUMBER(12),
   PRIMARY KEY (amout)
 );
