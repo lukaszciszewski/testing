5f10188bb1b6b59a064d4979cf3e6fe875a762cb
Łukasz Ciszewski <ciszewski@tkomp.pl>
2017-04-25 08:26:14 +0200

    commit

--- LUKE/Views/XX_BJO2.sql
+++ LUKE/Views/XX_BJO2.sql
@@ -1,4 +1,4 @@
 CREATE OR REPLACE FORCE VIEW luke.xx_bjo2 ("OWNER",table_name,tablespace_name) AS
 select "OWNER", "TABLE_NAME", "TABLESPACE_NAME"
 from ALL_TABLES
-where table_name like 'bin%';
+where table_name like ' bin%' or table_name ='test t43 gg';
