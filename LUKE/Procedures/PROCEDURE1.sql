6c1c56aefe8a264a6c5040654e7c9ef043cb4277
Łukasz Ciszewski <ciszewski@tkomp.pl>
2017-04-28 13:32:44 +0200

    delete

--- LUKE/Procedures/PROCEDURE1.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE procedure luke.procedure1 as
-a varchar2(100);
-begin
-  a := ' Info: ';
-end procedure1;
-/
--- LUKE/Views/XX_BJO.sql
+++
@@ -1,4 +0,0 @@
-CREATE OR REPLACE FORCE VIEW luke.xx_bjo ("OWNER",table_name,tablespace_name) AS
-select "OWNER", "TABLE_NAME", "TABLESPACE_NAME"
-from ALL_TABLES
-where table_name like 'bin%';
